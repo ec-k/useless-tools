30c25ba2700f7e7355a9573d8cc96b86cdcbdc1f
EC-K <elxone.19jt@gmail.com>
2025-08-24 13:04:53 +0900

    init

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"slices"
+)
+
+func createCharacterSet(s string) []rune {
+	runes := []rune(s)
+	slices.Sort(runes)
+	unique := slices.Compact(runes)
+	return unique
+}
+
+func randomizeString(raw_string string) string {
+	length := len([]rune(raw_string))
+	uniqueStringSet := createCharacterSet(raw_string)
+
+	randomizedString := make([]rune, length)
+	for i:=0;i<length;i++{
+		runeIndex := rand.Intn(len(uniqueStringSet))
+		randomRune := uniqueStringSet[runeIndex]
+		randomizedString[i] = randomRune
+	}
+
+	return string(randomizedString)
+}
+
+func parseArgs() string {
+	return os.Args[1]
+}
+
+func main(){
+	var inputString string = parseArgs()
+	fmt.Println(randomizeString(inputString))
+}
+
